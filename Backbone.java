e6da63b4685b2d9c25f9c3ca88355daa71a801e6
Meyer07 <meyer483@uwm.edu>
2025-03-06 19:55:59 -0600

    Add files via upload

---
+++ Backbone.java
@@ -0,0 +1,18 @@
+import java.util.Math;
+
+public class Backbone 
+{
+ private int currentIndex;	
+ private int numItems;	
+ private int lifeStart=40;
+ private int []lifeArray;
+ private int INITIAL_CAPACITY=50;
+    public Backbone() 
+    {
+    	numItems=0;
+    	currentIndex=0;
+    	lifeArray=new int[INITIAL_CAPACITY];
+    }
+    
+    
+}
