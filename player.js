bb59ae9d5af92958a81fe7fe51a90f1337c41ce4
Sam Meyer <meyer483@uwm.edu>
2025-12-06 12:37:03 -0600

    converted the java file into a javascript file

---
+++ player.js
@@ -0,0 +1,100 @@
+export class Player
+{
+    constructor(startingLfe,name)
+    {
+        this.life=startingLife;
+        this.name=name;
+        this.isDead=false;
+        this.commanderDmg=[];
+        this.poisoncnt=0;
+
+    }
+
+    numPlayers(n)
+    {
+        this.commanderDmg=new Array(n).fill(0);
+    }
+
+    getLife()
+    {
+        return this.life;
+    }
+
+    getName()
+    {
+        return this.name;
+    }
+    gainLife(n)
+    {
+        if(this.isDead)
+            return;
+        this.life+=n;
+    }
+
+    takeDmg(dmg)
+    {
+        if(this.isDead)
+            return;
+        this.life-=dmg;
+        this.checkLoss();
+    }
+    takeCommandDmg(cdmg,p)
+    {
+        if(this.isDead)
+            return;
+        this.commanderDmg[p]+=cdmg;
+        this.takeDmg(cdmg);
+    }
+
+    removeCommandmg(cdmg,p)
+    {
+        if(this.isDead)
+            return;
+        this.commanderDmg[p]-=cdmg;
+        this.gainLife(cdmg);
+        if(this.commanderDmg[p]<0) this.commanderDmg[p]=0;
+    }
+    takePoison(pdmg)
+    {
+        if(this.isDead)
+            return;
+        this.poisoncnt+=pdmg;
+        this.checkLoss();
+    }
+    removePoison(pdmg)
+    {
+        if(this.isDead)
+            return;
+        this.poisoncnt-=pdmg;
+        if(this.poisoncnt<0)
+        {
+            this.poisoncnt=0;
+        }        
+    }
+    getIsDead()
+    {
+        return this.isDead;
+    }
+
+    checkLoss()
+    {
+        if(this.isDead)
+        {
+            return;
+        }
+        if(this.life<=0 || this.poisoncnt>=10)
+        {
+            this.isDead=true;
+        }
+
+        //for loop to check the commander damage stat
+        for(let i=0; i<this.commanderDmg.length();i++)
+        {
+            if(this.commanderDmg>=21)
+            {
+                this.isDead=true;
+            }
+        }
+    }
+
+}
